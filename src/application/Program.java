f8342fdbd575c56e36c19970ac44d308baef0b2d
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-02-07 16:18:50 -0300

    Moving and treating exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         ChessMatch match = new ChessMatch();
-        UI.printBoard(match.getPieces());
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = match.performChessMove(source, target);
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
     }
 }
