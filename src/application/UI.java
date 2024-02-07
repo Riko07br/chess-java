95e28b697326e2259a7575b0a920c37ab174f6f2
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-02-06 22:11:37 -0300

    improve ui with colors

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,31 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+
+    //region Colors
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    //endregion
+
     public static void printBoard(ChessPiece[][] chessPieces) {
         for (int i = 0; i < chessPieces.length; i++) {
             //print row id
@@ -21,7 +44,11 @@ public class UI {
         if (chessPiece == null) {
             System.out.print("-");
         } else {
-            System.out.print(chessPiece);
+            if (chessPiece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + chessPiece + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_YELLOW + chessPiece + ANSI_RESET);
+            }
         }
         //Extra space
         System.out.print(" ");
